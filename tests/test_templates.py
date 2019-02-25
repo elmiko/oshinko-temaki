99b01b068ed66ff52113cc3ec1d17e9a380f7678
Michael McCune <msm@redhat.com>
2019-02-25 17:44:42 -0500

    add minimal CRDTemplate test
    Also clean up a yaml warning.

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -22,7 +22,7 @@ class TestCMTemplate(unittest.TestCase):
 
 
     def test_minimum(self):
-        """test the small possible definition of a cluster"""
+        """test the small possible ConfigMap definition of a cluster"""
         expected = self.base_expected()
         expected["metadata"]["name"] = "test-cluster"
         expected["data"]["config"] = {
@@ -37,6 +37,39 @@ class TestCMTemplate(unittest.TestCase):
             image=None,
             metrics=False).dumps()
         observed = json.loads(raw)
-        observed["data"]["config"] = yaml.load(observed["data"]["config"])
+        observed["data"]["config"] = yaml.load(observed["data"]["config"],
+                                               Loader=yaml.FullLoader)
+        self.assertDictEqual(observed, expected)
+
+
+class TestCRDTemplate(unittest.TestCase):
+    @staticmethod
+    def base_expected():
+        return {
+            "apiVersion": "radanalytics.io/v1",
+            "kind": "SparkCluster",
+            "metadata": {},
+            "spec": {
+                "master": {},
+                "worker": {}
+            }
+        }
+
+    def test_minimum(self):
+        """test the small possible CRD definition of a cluster"""
+        expected = self.base_expected()
+        expected["metadata"]["name"] = "test-cluster"
+        expected["spec"] = {
+            "master": { "instances": 1 },
+            "worker": { "instances": 1 }
+        }
+
+        raw = templates.CRDTemplate(
+            name="test-cluster",
+            masters=1,
+            workers=1,
+            image=None,
+            metrics=False).dumps()
+        observed = json.loads(raw)
         self.assertDictEqual(observed, expected)
 
